d38d6c513f62c79cdda86a05f0aa43313ade99ad
github username <github email address>
2020-06-06 21:53:19 -0400

    add initial template fetch request

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log("this works")
+const baseURL = 'http://localhost:3000/api/v1/'
+const templatesURL = `${baseURL}/templates`
+const verbsURL = `${baseURL}/verbs`
+const nounsURL = `${baseURL}/nouns`
+const adjectivesURL = `${baseURL}/adjectives`
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchTemplates()
+    // getQuoteInfo()
+})
+
+function fetchTemplates() {
+    fetch(templatesURL)
+    .then(resp => resp.json())
+    .then(templates => {
+        for (const template of templates){
+            renderRandomTemplate(template)
+        }
+    })
+}
+
+// RENDERS RANDOM TEMPLATE 
+function renderRandomTemplate(template) {
+    //make quote box
+    // const quoteBox = document.createElement('div')
+    // quoteBox.classList += "quoteBox" 
+    // quoteBox.dataset["id"] = template.id 
+
+    let randomTemplate = selectRandom(template)
+    console.log(randomTemplate)
+    // let quote = data.content
+    // let templateId = template.id
+    // console.log(quote)
+
+    // form.innerHTML = replaceText(quote, data)
+    // formHandler(quote, templateId)
+}
+
+// RANDOMIZER
+function selectRandom(data) {
+    return data[Math.floor(Math.random() * data.length)];
+}
+
+
+
