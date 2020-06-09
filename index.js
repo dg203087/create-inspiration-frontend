973275f6ab6ee361f719115c0e57f2edc1cbd58c
github username <github email address>
2020-06-08 23:33:27 -0400

    add image rendering

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const baseURL = 'http://localhost:3000/api/v1/'
 const templatesURL = `${baseURL}/templates`
-const verbsURL = `${baseURL}/verbs`
-const nounsURL = `${baseURL}/nouns`
-const adjectivesURL = `${baseURL}/adjectives`
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchRandomTemplate()
     // getQuoteInfo()
     const addWordsForm = document.querySelector('#add-words-form')
     addWordsForm.addEventListener('submit', (e) => createFormHandler(e)) //create event handler to make sure page doesn't refresh
-
 })
 
 // FETCH RANDOM TEMPLATE 
@@ -19,19 +15,44 @@ function fetchRandomTemplate() {
     .then(templates => {
         let randomTemplateId = templates[Math.floor(Math.random() * templates.length)]
         let randomTemplateContent = randomTemplateId.content
+        let templateImage = randomTemplateId.image_url
         // renderTemplate(randomTemplateContent) 
         const currentTemplate = `
         <div data-id=${randomTemplateId.id}> 
-        <h3>${randomTemplateContent}</h3>
-        <button>Submit</button>
+          <img src=${templateImage} height="350", width="400">
+          <div class="centered"><h3>${randomTemplateContent}</h3></div>
+        </div>
         `
         document.querySelector('.template-container').innerHTML += currentTemplate
     })
 }
 
-function createFormHandler(e) { //grabbing all input values
+function createFormHandler(e) { //grabbing all input values 
     e.preventDefault()
-    
+    const nounInput = document.querySelector('#input-noun').value
+    const verbInput = document.querySelector('#input-verb').value
+    const adjectiveInput = document.querySelector('#input-adjective').value
+    postFetch(nounInput, verbInput, adjectiveInput) 
+}
+
+// POST 
+function postFetch(noun, verb, adjective){
+    // const quoteData = {noun, verb, adjective}
+    // const noun = noun
+    // const verb = verb
+    // const adjective = adjective
+
+  fetch(endPoint, {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(quoteData)
+  })
+  .then(response => response.json())
+  // .catch(err => console.log(err))
+  .then(quote => {
+    console.log(quote);
+   
+  })
 }
 
 
