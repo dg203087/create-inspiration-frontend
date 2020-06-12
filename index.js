cdb0caf7ec47218d9ec8b6325ae09884d1d04303
github username <github email address>
2020-06-11 23:45:49 -0400

    fetch and post template to quote

--- index.js
+++ index.js
@@ -1,12 +1,85 @@
-const baseURL = 'http://localhost:3000/api/v1/'
+const baseURL = 'http://localhost:3000/api/v1'
 const templatesURL = `${baseURL}/templates`
+// const nounsURL = `${baseURL}/nouns`
+// const verbsURL = `${baseURL}/verbs`
+// const adjectivesURL = `${baseURL}/adjectives`
+const quotesURL = `${baseURL}/quotes`
+let indexBtn 
+let refreshBtn
+let postBtn
+// let indexContainer
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchRandomTemplate()
-    // getQuoteInfo()
     const addWordsForm = document.querySelector('#add-words-form')
     addWordsForm.addEventListener('submit', (e) => createFormHandler(e)) //create event handler to make sure page doesn't refresh
+
+    indexBtn = document.querySelector("#index-button")
+    indexBtn.addEventListener("click", fetchQuotes)
+      // console.log("this button works")
+
+    refreshBtn = document.querySelector("#refresh-button")
+    refreshBtn.addEventListener("click", function() {
+      console.log("this button works")
+      // document.querySelector("#w3-container").innerHTML = "Hello World"
+      // fetch(quotesURL) 
+      //   .then(res => res.json())
+      //   .then(data => {
+      //     console.log(data)
+      // });
+      //   })
+    })
+
+    postBtn = document.querySelector("#post-button")
+    postBtn.addEventListener("click", function() {
+      console.log("this button works")
+      // document.querySelector("#w3-container").innerHTML = "Hello World"
+      // fetch(quotesURL) 
+      //   .then(res => res.json())
+      //   .then(data => {
+      //     console.log(data)
+      // });
+      //   })
+    })
 })
+
+function fetchQuotes(){
+  let indexContainer = document.querySelector("#w3-container")
+  indexContainer.innerHTML = "Hello World"
+}
+
+
+function createFormHandler(e) { //grabbing all input values 
+  e.preventDefault()
+  const nounInput = document.querySelector('#input-noun').value
+  const verbInput = document.querySelector('#input-verb').value
+  const adjectiveInput = document.querySelector('#input-adjective').value
+
+  createQuote(nounInput, verbInput, adjectiveInput) 
+}
+
+function createQuote(noun, verb, adjective) {
+  const nounData = {noun_word: noun}  //build noun object
+  const verbData = {verb: verb}
+  const adjData = {adjective: adjective}
+  
+  // const testSent = "Here lies a NOUN. It was super ADJECTIVE, but VERBed away."
+  // this is posting to a new quote, not UPDATING the existing one. PATCH? 
+  fetch(quotesURL, {
+    method: "POST",
+    headers: {"Content-Type": "application/json"}, 
+    body: JSON.stringify({
+      noun: nounData.noun_word,
+      verb: verbData.verb_word,
+      adjective: adjData.adjective_word
+    })
+  })
+  .then(response => response.json())
+  .then(quote => {
+    console.log(quote);
+  })
+  
+}
 
 // FETCH RANDOM TEMPLATE 
 function fetchRandomTemplate() {
@@ -16,58 +89,29 @@ function fetchRandomTemplate() {
         let randomTemplateId = templates[Math.floor(Math.random() * templates.length)]
         let randomTemplateContent = randomTemplateId.content
         let templateImage = randomTemplateId.image_url
-        // renderTemplate(randomTemplateContent) 
+    
         const currentTemplate = `
         <div data-id=${randomTemplateId.id}> 
-          <img src=${templateImage} height="350", width="400">
+          <img src=${templateImage} height="350", width="100%">
           <div class="centered"><h3>${randomTemplateContent}</h3></div>
         </div>
         `
         document.querySelector('.template-container').innerHTML += currentTemplate
+        postTemplate(randomTemplateId)
     })
 }
 
-function createFormHandler(e) { //grabbing all input values 
-    e.preventDefault()
-    const nounInput = document.querySelector('#input-noun').value
-    const verbInput = document.querySelector('#input-verb').value
-    const adjectiveInput = document.querySelector('#input-adjective').value
-    postFetch(nounInput, verbInput, adjectiveInput) 
-}
+// POSTS TEMPLATE TO QUOTE
+function postTemplate(tempID) {
+  const templateData = {template_id: tempID.id}
 
-// POST 
-function postFetch(noun, verb, adjective){
-    // const quoteData = {noun, verb, adjective}
-    // const noun = noun
-    // const verb = verb
-    // const adjective = adjective
-
-  fetch(endPoint, {
+  fetch(quotesURL, {
     method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify(quoteData)
+    headers: {"Content-Type": "application/json"}, 
+    body: JSON.stringify(templateData) // send data to API
   })
   .then(response => response.json())
-  // .catch(err => console.log(err))
   .then(quote => {
     console.log(quote);
-   
   })
 }
-
-
-// RENDER TEMPLATE REFACTOR 
-// function renderTemplate(template) {
-//     const currentTemplate = document.createElement('div')
-//     currentTemplate.classList += "template" 
-//     currentTemplate.dataset["id"] = template.id 
-
-//     document.querySelector('#template-container').innerHTML += currentTemplate
-
-//     form.innerHTML = replaceText(quote, data)
-//     formHandler(quote, templateId)
-// }
-
-
-
-
