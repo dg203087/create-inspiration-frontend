f4586315b9da01e99693210df6b2d2c85c5d46c8
dg203087 <50463967+dg203087@users.noreply.github.com>
2020-06-21 22:01:25 -0400

    refactor render quote function from index.js

--- quote.js
+++ quote.js
@@ -1,10 +1,25 @@
 class Quote {
-    constructor(id, quoteAttributes) {
-        this.id = id
+    constructor(quoteAttributes) {
+        this.id = quoteAttributes.id
         this.adjective = quoteAttributes.adjective
         this.verb = quoteAttributes.verb
         this.noun = quoteAttributes.noun
         this.image_url = quoteAttributes.template.image_url
-        this.content = syllabusAttributes.template.content
+        this.content = quoteAttributes.template.content
+        this.newQuote = newQuote
         Quote.all.push(this);
+    }
+        
+     renderQuote() {
+        return `
+        <div data-id=${this.id}> 
+          <img src=${this.image_url} height="350", width="100%">
+          <div class="bottom-centered"><h3>${this.newQuote}</h3></div>
+        </div>
+        <br><br>
+        `
+     }
 }
+
+//array of all quote objects
+Quote.all = []
